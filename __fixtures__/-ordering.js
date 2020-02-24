795461afef00455997bf3f98235aa676252272bf
Ben Rogerson <info@benrogerson.com.au>
2020-02-24 21:47:04 +1030

    update tests

---
+++ __fixtures__/-ordering.js
@@ -0,0 +1,5 @@
+import tw from './macro'
+
+// This tests the screens end up in the order they are defined in the config
+
+tw.div`xl:bg-red-500 lg:bg-blue-500 fill-current md:bg-pink-500 sm:bg-orange-500 sm:text-yellow-500 hidden`
