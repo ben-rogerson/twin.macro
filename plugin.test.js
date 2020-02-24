2335049bb881db100d675b7188a0feebd36ad05e
Ben Rogerson <info@benrogerson.com.au>
2020-02-25 06:51:21 +1030

    update babel-plugin-tester

--- plugin.test.js
+++ plugin.test.js
@@ -1,4 +1,4 @@
-const pluginTester = require('babel-plugin-tester')
+const pluginTester = require('babel-plugin-tester').default
 let plugin = require('babel-plugin-macros')
 let path = require('path')
 let glob = require('glob-all')
@@ -13,15 +13,6 @@ pluginTester({
     babelrc: true
   },
   snapshot: true,
-  formatResult(code) {
-    return prettier
-      .format(code, {
-        parser: 'babel',
-        semi: false,
-        singleQuote: true
-      })
-      .trim()
-  },
   tests: glob.sync('__fixtures__/*.js').map(file => ({
     title: path.basename(file),
     code: fs.readFileSync(file, 'utf-8')
