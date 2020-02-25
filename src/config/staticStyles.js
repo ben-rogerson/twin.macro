f931cb91c6a19c34fa20bb949ebc32b8d1aa9e28
Ben Rogerson <info@benrogerson.com.au>
2020-02-26 00:09:25 +1030

    fix initial scale transforms

--- src/config/staticStyles.js
+++ src/config/staticStyles.js
@@ -490,8 +490,8 @@ const staticStyles = {
       '--transform-rotate': '0',
       '--transform-skew-x': '0',
       '--transform-skew-y': '0',
-      '--transform-scale-x': '0',
-      '--transform-scale-y': '0',
+      '--transform-scale-x': '1',
+      '--transform-scale-y': '1',
       transform:
         'translateX(var(--transform-translate-x)) translateY(var(--transform-translate-y)) rotate(var(--transform-rotate)) skewX(var(--transform-skew-x)) skewY(var(--transform-skew-y)) scaleX(var(--transform-scale-x)) scaleY(var(--transform-scale-y))'
     },
