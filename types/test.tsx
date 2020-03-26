450dcb1e79e7923c07ed3b630baa129cc425f2c2
kingdaro <kingdaro@gmail.com>
2020-03-26 14:53:18 -0500

    add types, tests and related dependencies

---
+++ types/test.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import tw from '.'
+
+tw`bg-blue-500 text-gray-100`
+
+// @ts-expect-error
+tw('')
+
+// @ts-expect-error
+tw`interpolations not supported ${123}`
+
+tw.button`bg-blue-500 text-gray-100`
+tw.a`bg-blue-500 text-gray-100`
+tw.p`bg-blue-500 text-gray-100`
+tw.img`bg-blue-500 text-gray-100`
+
+// @ts-expect-error
+tw.nonexistentelement``
+
+// @ts-expect-error
+tw('call syntax not supported')``
+
+function App() {
+  return <p>hi</p>
+}
+
+const twPropElement = <div tw="prop" />
+const twPropComponent = <App tw="prop" />
