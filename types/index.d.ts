99ed2c7b8cf10ee0932d0753cd55c1229dee454e
kingdaro <kingdaro@gmail.com>
2020-03-31 09:57:05 -0500

    even looser CSSObject type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,7 @@
-import * as CSS from 'csstype'
 import 'react'
 
-// "borrowed" from styled components types
-// this seems to work with emotion and SC so we'll use this
-export type CSSProperties = CSS.Properties<string | number>
-
-export type CSSPseudos = { [K in CSS.Pseudos]?: CSSObject }
-
-export interface CSSObject extends CSSProperties, CSSPseudos {
-  [key: string]: CSSObject | string | number | undefined
+export interface CSSObject {
+  [key: string]: string | number | CSSObject
 }
 
 export type TwFn = (
