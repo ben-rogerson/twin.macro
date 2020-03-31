d31d9f5f2e88a5bd656188c923fb6263dea0e8c3
kingdaro <kingdaro@gmail.com>
2020-03-31 09:37:03 -0500

    new CSSObject type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,22 @@
-import { CSSObject } from '@emotion/serialize'
+import * as CSS from 'csstype'
 import 'react'
 
-type TwFn = (strings: TemplateStringsArray, ...values: never[]) => CSSObject
+// "borrowed" from styled components types
+// this seems to work with emotion and SC so we'll use this
+export type CSSProperties = CSS.Properties<string | number>
+
+export type CSSPseudos = { [K in CSS.Pseudos]?: CSSObject }
+
+export interface CSSObject extends CSSProperties, CSSPseudos {
+  [key: string]: CSSObject | string | number | undefined
+}
+
+export type TwFn = (
+  strings: TemplateStringsArray,
+  ...values: never[]
+) => CSSObject
 
 declare const tw: TwFn & Record<keyof JSX.IntrinsicElements, TwFn>
-
 export default tw
 
 declare module 'react' {
