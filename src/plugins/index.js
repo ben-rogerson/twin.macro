2f0e720d05d958ff45e59a28997f676527816703
Ben Rogerson <info@benrogerson.com.au>
2020-04-29 21:29:51 +0930

    add plugin index

---
+++ src/plugins/index.js
@@ -0,0 +1,4 @@
+export { default as container } from './container'
+export { default as divide } from './divide'
+export { default as space } from './space'
+export { default as text } from './text'
