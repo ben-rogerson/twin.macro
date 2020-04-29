23edd2fea1238e2c8a9411fb23266e14fdc855af
Ben Rogerson <info@benrogerson.com.au>
2020-04-29 21:24:54 +0930

    add placeholder opacity class

--- src/config/dynamicStyles.js
+++ src/config/dynamicStyles.js
@@ -177,6 +177,11 @@ export default {
   // See staticStyles.js
 
   // https://tailwindcss.com/docs/placeholder-color
+  'placeholder-opacity': {
+    prop: '--placeholder-opacity',
+    config: 'placeholderOpacity',
+    configFallback: 'opacity',
+  },
   placeholder: { prop: 'color', config: 'placeholderColor' },
 
   // https://tailwindcss.com/docs/text-align
