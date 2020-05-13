d09d9532f3dd75786b01ea6b6e1c2e1d008abba0
Ben Rogerson <info@benrogerson.com.au>
2020-05-13 19:38:25 +0930

    remove alpha fallback
    this was causing specificity issues

--- src/utils/withAlpha.js
+++ src/utils/withAlpha.js
@@ -25,13 +25,7 @@ export default ({ color, property, variable, important }) => {
 
     return {
       [variable]: '1',
-      // Duplicate keys aren't possible in js objects, but totally fine in css
-      // (eg: { "color": "#000", "color": "rgba(255,255,255,.5)" } = error
-      // So nesting the duplicate key under the & is the workaround
-      [property]: colorValue,
-      '&': {
-        [property]: `rgba(${r}, ${g}, ${b}, var(${variable}))${important}`,
-      },
+      [property]: `rgba(${r}, ${g}, ${b}, var(${variable}))${important}`,
     }
   } catch (_) {
     return {
