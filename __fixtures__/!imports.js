0319744700b03ab4bc222b1aaf0a122c9ab12ead
Ben Rogerson <info@benrogerson.com.au>
2020-05-18 09:03:32 +0930

    add import tests

---
+++ __fixtures__/!imports.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import tw, { styled, css } from './macro'
+
+const twPropertyTest = <div tw="text-purple-500" />
+
+const twFunctionTest = tw.div`text-purple-500`
+
+const cssFunctionTest = (
+  <div
+    css={css`
+      ${tw`text-purple-500`}
+      background-color: purple;
+    `}
+  />
+)
+
+const styledFunctionTest = styled.div`
+  ${css`
+    ${tw`text-purple-500`}
+    background-color: purple;
+  `}
+`
