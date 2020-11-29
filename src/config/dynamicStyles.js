ben-rogerson/twin.macro

commit cb96a1c55b872c237bf91e3e4550c2959ab88cf2

add css variable prefixes

src/config/dynamicStyles.js
@@ -203,7 +203,7 @@ export default {
   // https://tailwindcss.com/docs/text-color
   // https://tailwindcss.com/docs/font-size
   'text-opacity': {
-    prop: '--text-opacity',
+    prop: '--tw-text-opacity',
     config: 'textOpacity',
     configFallback: 'opacity',
   },
@@ -228,7 +228,7 @@ export default {
 
   // https://tailwindcss.com/docs/background-opacity
   'bg-opacity': {
-    prop: '--bg-opacity',
+    prop: '--tw-bg-opacity',
     config: 'backgroundOpacity',
     configFallback: 'opacity',
   },
@@ -255,7 +255,7 @@ export default {
   'border-r': { prop: 'borderRightWidth', config: 'borderWidth' },
 
   'border-opacity': {
-    prop: '--border-opacity',
+    prop: '--tw-border-opacity',
     config: 'borderOpacity',
     configFallback: 'opacity',
   },
@@ -330,23 +330,23 @@ export default {
    */
 
   // https://tailwindcss.com/docs/scale
-  'scale-x': { prop: '--transform-scale-x', config: 'scale' },
-  'scale-y': { prop: '--transform-scale-y', config: 'scale' },
+  'scale-x': { prop: '--tw-transform-scale-x', config: 'scale' },
+  'scale-y': { prop: '--tw-transform-scale-y', config: 'scale' },
   scale: {
-    prop: ['--transform-scale-x', '--transform-scale-y'],
+    prop: ['--tw-transform-scale-x', '--tw-transform-scale-y'],
     config: 'scale',
   },
 
   // https://tailwindcss.com/docs/rotate
-  rotate: { prop: '--transform-rotate', config: 'rotate' },
+  rotate: { prop: '--tw-rotate', config: 'rotate' },
 
   // https://tailwindcss.com/docs/translate
-  'translate-x': { prop: '--transform-translate-x', config: 'translate' },
-  'translate-y': { prop: '--transform-translate-y', config: 'translate' },
+  'translate-x': { prop: '--tw-translate-x', config: 'translate' },
+  'translate-y': { prop: '--tw-translate-y', config: 'translate' },
 
   // https://tailwindcss.com/docs/skew
-  'skew-x': { prop: '--transform-skew-x', config: 'skew' },
-  'skew-y': { prop: '--transform-skew-y', config: 'skew' },
+  'skew-x': { prop: '--tw-skew-x', config: 'skew' },
+  'skew-y': { prop: '--tw-skew-y', config: 'skew' },
 
   // https://tailwindcss.com/docs/transform-origin
   origin: { prop: 'transformOrigin', config: 'transformOrigin' },

src/config/staticStyles.js
@@ -2,13 +2,13 @@
 // This feature uses var+comment hacks to get around property stripping:
 // https://github.com/tailwindlabs/tailwindcss.com/issues/522#issuecomment-687667238
 const fontVariants = {
-  '--font-variant-numeric-ordinal': 'var(--twin-empty,/*!*/ /*!*/)',
-  '--font-variant-numeric-slashed-zero': 'var(--twin-empty,/*!*/ /*!*/)',
-  '--font-variant-numeric-figure': 'var(--twin-empty,/*!*/ /*!*/)',
-  '--font-variant-numeric-spacing': 'var(--twin-empty,/*!*/ /*!*/)',
-  '--font-variant-numeric-fraction': 'var(--twin-empty,/*!*/ /*!*/)',
+  '--tw-ordinal': 'var(--tw-empty,/*!*/ /*!*/)',
+  '--tw-slashed-zero': 'var(--tw-empty,/*!*/ /*!*/)',
+  '--tw-numeric-figure': 'var(--tw-empty,/*!*/ /*!*/)',
+  '--tw-numeric-spacing': 'var(--tw-empty,/*!*/ /*!*/)',
+  '--tw-numeric-fraction': 'var(--tw-empty,/*!*/ /*!*/)',
   fontVariantNumeric:
-    'var(--font-variant-numeric-ordinal) var(--font-variant-numeric-slashed-zero) var(--font-variant-numeric-figure) var(--font-variant-numeric-spacing) var(--font-variant-numeric-fraction)',
+    'var(--tw-ordinal) var(--tw-slashed-zero) var(--tw-numeric-figure) var(--tw-numeric-spacing) var(--tw-numeric-fraction)',
 }
 
 export default {
@@ -120,14 +120,14 @@ export default {
   'space-x-reverse': {
     output: {
       '> :not(template) ~ :not(template)': {
-        '--space-x-reverse': 1,
+        '--tw-space-x-reverse': 1,
       },
     },
   },
   'space-y-reverse': {
     output: {
       '> :not(template) ~ :not(template)': {
-        '--space-y-reverse': 1,
+        '--tw-space-y-reverse': 1,
       },
     },
   },
@@ -137,14 +137,14 @@ export default {
   'divide-x-reverse': {
     output: {
       '> :not(template) ~ :not(template)': {
-        '--divide-x-reverse': 1,
+        '--tw-divide-x-reverse': 1,
       },
     },
   },
   'divide-y-reverse': {
     output: {
       '> :not(template) ~ :not(template)': {
-        '--divide-y-reverse': 1,
+        '--tw-divide-y-reverse': 1,
       },
     },
   },
@@ -339,45 +339,45 @@ export default {
 
   // https://tailwindcss.com/docs/font-variant-numeric
   ordinal: {
-    output: { ...fontVariants, '--font-variant-numeric-ordinal': 'ordinal' },
+    output: { ...fontVariants, '--tw-ordinal': 'ordinal' },
   },
   'slashed-zero': {
     output: {
       ...fontVariants,
-      '--font-variant-numeric-slashed-zero': 'slashed-zero',
+      '--tw-slashed-zero': 'slashed-zero',
     },
   },
   'lining-nums': {
-    output: { ...fontVariants, '--font-variant-numeric-figure': 'lining-nums' },
+    output: { ...fontVariants, '--tw-numeric-figure': 'lining-nums' },
   },
   'oldstyle-nums': {
     output: {
       ...fontVariants,
-      '--font-variant-numeric-figure': 'oldstyle-nums',
+      '--tw-numeric-figure': 'oldstyle-nums',
     },
   },
   'proportional-nums': {
     output: {
       ...fontVariants,
-      '--font-variant-numeric-spacing': 'proportional-nums',
+      '--tw-numeric-spacing': 'proportional-nums',
     },
   },
   'tabular-nums': {
     output: {
       ...fontVariants,
-      '--font-variant-numeric-spacing': 'tabular-nums',
+      '--tw-numeric-spacing': 'tabular-nums',
     },
   },
   'diagonal-fractions': {
     output: {
       ...fontVariants,
-      '--font-variant-numeric-fraction': 'diagonal-fractions',
+      '--tw-numeric-fraction': 'diagonal-fractions',
     },
   },
   'stacked-fractions': {
     output: {
       ...fontVariants,
-      '--font-variant-numeric-fraction': 'stacked-fractions',
+      '--tw-numeric-fraction': 'stacked-fractions',
     },
   },
   'normal-nums': {
@@ -700,15 +700,15 @@ export default {
 
   transform: {
     output: {
-      '--transform-translate-x': '0',
-      '--transform-translate-y': '0',
-      '--transform-rotate': '0',
-      '--transform-skew-x': '0',
-      '--transform-skew-y': '0',
-      '--transform-scale-x': '1',
-      '--transform-scale-y': '1',
+      '--tw-translate-x': '0',
+      '--tw-translate-y': '0',
+      '--tw-rotate': '0',
+      '--tw-skew-x': '0',
+      '--tw-skew-y': '0',
+      '--tw-scale-x': '1',
+      '--tw-scale-y': '1',
       transform:
-        'translateX(var(--transform-translate-x)) translateY(var(--transform-translate-y)) rotate(var(--transform-rotate)) skewX(var(--transform-skew-x)) skewY(var(--transform-skew-y)) scaleX(var(--transform-scale-x)) scaleY(var(--transform-scale-y))',
+        'translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))',
     },
     config: false,
   },

src/plugins/bg.js
@@ -7,7 +7,7 @@ const handleColor = ({ configValue, important, disableColorVariables }) => {
   return withAlpha({
     color: value,
     property: 'backgroundColor',
-    variable: !disableColorVariables && '--bg-opacity',
+    variable: !disableColorVariables && '--tw-bg-opacity',
     important,
   })
 }

src/plugins/border.js
@@ -16,7 +16,7 @@ const handleColor = ({ configValue, important, disableColorVariables }) => {
   return withAlpha({
     color: value,
     property: 'borderColor',
-    variable: !disableColorVariables && '--border-opacity',
+    variable: !disableColorVariables && '--tw-border-opacity',
     important,
   })
 }

src/plugins/divide.js
@@ -10,7 +10,7 @@ const handleColor = ({ configValue, important, disableColorVariables }) => {
   const borderColor = withAlpha({
     color: value,
     property: 'borderColor',
-    variable: !disableColorVariables && '--divide-opacity',
+    variable: !disableColorVariables && '--tw-divide-opacity',
     important,
   })
 
@@ -22,7 +22,9 @@ const handleOpacity = ({ configValue }) => {
   if (!opacity) return
 
   return {
-    '> :not(template) ~ :not(template)': { '--divide-opacity': `${opacity}` },
+    '> :not(template) ~ :not(template)': {
+      '--tw-divide-opacity': `${opacity}`,
+    },
   }
 }
 
@@ -35,7 +37,9 @@ const handleWidth = ({
 
   const value = `${negative}${addPxTo0(width)}`
   const isDivideX = className.startsWith('divide-x')
-  const cssVariableKey = isDivideX ? '--divide-x-reverse' : '--divide-y-reverse'
+  const cssVariableKey = isDivideX
+    ? '--tw-divide-x-reverse'
+    : '--tw-divide-y-reverse'
 
   const borderFirst = `calc(${value} * var(${cssVariableKey}))${important}`
   const borderSecond = `calc(${value} * calc(1 - var(${cssVariableKey})))${important}`

src/plugins/gradient.js
@@ -35,19 +35,19 @@ export default properties => {
 
   const styleDefinitions = {
     from: {
-      '--gradient-from-color': getColorValue(value, 'from'),
-      '--gradient-color-stops': `var(--gradient-from-color), var(--gradient-to-color, ${transparentTo(
+      '--tw-gradient-from': getColorValue(value, 'from'),
+      '--tw-gradient-stops': `var(--tw-gradient-from), var(--tw-gradient-to, ${transparentTo(
         value
       )})`,
     },
     via: {
-      '--gradient-via-color': getColorValue(value, 'via'),
-      '--gradient-color-stops': `var(--gradient-from-color), var(--gradient-via-color), var(--gradient-to-color, ${transparentTo(
-        value
-      )})`,
+      '--tw-gradient-stops': `var(--tw-gradient-from), ${getColorValue(
+        value,
+        'via'
+      )}, var(--tw-gradient-to, ${transparentTo(value)})`,
     },
     to: {
-      '--gradient-to-color': getColorValue(value, 'to'),
+      '--tw-gradient-to': getColorValue(value, 'to'),
     },
   }
 

src/plugins/placeholder.js
@@ -7,7 +7,7 @@ const handleColor = ({ configValue, important, disableColorVariables }) => {
   return withAlpha({
     color: value,
     property: 'color',
-    variable: !disableColorVariables && '--placeholder-opacity',
+    variable: !disableColorVariables && '--tw-placeholder-opacity',
     important,
   })
 }
@@ -16,7 +16,7 @@ const handleOpacity = ({ configValue }) => {
   const value = configValue('placeholderOpacity') || configValue('opacity')
   if (!value) return
 
-  return { '--placeholder-opacity': `${value}` }
+  return { '--tw-placeholder-opacity': `${value}` }
 }
 
 export default properties => {

commit 5ab6181b02a324bc5edba52e244574222748d42f

bugfix: transform scale variables
Tailwind v2 uses --tw-scale-x and --tw-scale-y for transforms.
Commit cb96a1c accidentally introduced --tw-transform-scale-x
and --tw-transform-scale-y, which are not set, turning scale
transitions into a nop.

__snapshots__/plugin.test.js.snap
@@ -17349,15 +17349,15 @@ tw\`-skew-y-1\`
     'translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))',
 })
 ;({
-  '--transform-translate-x': '0',
-  '--transform-translate-y': '0',
-  '--transform-rotate': '0',
-  '--transform-skew-x': '0',
-  '--transform-skew-y': '0',
-  '--transform-scale-x': '1',
-  '--transform-scale-y': '1',
+  '--tw-translate-x': '0',
+  '--tw-translate-y': '0',
+  '--tw-rotate': '0',
+  '--tw-skew-x': '0',
+  '--tw-skew-y': '0',
+  '--tw-scale-x': '1',
+  '--tw-scale-y': '1',
   transform:
-    'translate3d(var(--transform-translate-x), var(--transform-translate-y), 0) rotate(var(--transform-rotate)) skewX(var(--transform-skew-x)) skewY(var(--transform-skew-y)) scaleX(var(--transform-scale-x)) scaleY(var(--transform-scale-y))',
+    'translate3d(var(--tw-translate-x), var(--tw-translate-y), 0) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))',
 })
 ;({
   transform: 'none',
@@ -17392,104 +17392,104 @@ tw\`-skew-y-1\`
 }) // https://tailwindcss.com/docs/scale
 
 ;({
-  '--tw-transform-scale-x': '0',
-  '--tw-transform-scale-y': '0',
+  '--tw-scale-x': '0',
+  '--tw-scale-y': '0',
 })
 ;({
-  '--tw-transform-scale-x': '.5',
-  '--tw-transform-scale-y': '.5',
+  '--tw-scale-x': '.5',
+  '--tw-scale-y': '.5',
 })
 ;({
-  '--tw-transform-scale-x': '.75',
-  '--tw-transform-scale-y': '.75',
+  '--tw-scale-x': '.75',
+  '--tw-scale-y': '.75',
 })
 ;({
-  '--tw-transform-scale-x': '.9',
-  '--tw-transform-scale-y': '.9',
+  '--tw-scale-x': '.9',
+  '--tw-scale-y': '.9',
 })
 ;({
-  '--tw-transform-scale-x': '.95',
-  '--tw-transform-scale-y': '.95',
+  '--tw-scale-x': '.95',
+  '--tw-scale-y': '.95',
 })
 ;({
-  '--tw-transform-scale-x': '1',
-  '--tw-transform-scale-y': '1',
+  '--tw-scale-x': '1',
+  '--tw-scale-y': '1',
 })
 ;({
-  '--tw-transform-scale-x': '1.05',
-  '--tw-transform-scale-y': '1.05',
+  '--tw-scale-x': '1.05',
+  '--tw-scale-y': '1.05',
 })
 ;({
-  '--tw-transform-scale-x': '1.1',
-  '--tw-transform-scale-y': '1.1',
+  '--tw-scale-x': '1.1',
+  '--tw-scale-y': '1.1',
 })
 ;({
-  '--tw-transform-scale-x': '1.25',
-  '--tw-transform-scale-y': '1.25',
+  '--tw-scale-x': '1.25',
+  '--tw-scale-y': '1.25',
 })
 ;({
-  '--tw-transform-scale-x': '1.5',
-  '--tw-transform-scale-y': '1.5',
+  '--tw-scale-x': '1.5',
+  '--tw-scale-y': '1.5',
 })
 ;({
-  '--tw-transform-scale-x': '0',
+  '--tw-scale-x': '0',
 })
 ;({
-  '--tw-transform-scale-x': '.5',
+  '--tw-scale-x': '.5',
 })
 ;({
-  '--tw-transform-scale-x': '.75',
+  '--tw-scale-x': '.75',
 })
 ;({
-  '--tw-transform-scale-x': '.9',
+  '--tw-scale-x': '.9',
 })
 ;({
-  '--tw-transform-scale-x': '.95',
+  '--tw-scale-x': '.95',
 })
 ;({
-  '--tw-transform-scale-x': '1',
+  '--tw-scale-x': '1',
 })
 ;({
-  '--tw-transform-scale-x': '1.05',
+  '--tw-scale-x': '1.05',
 })
 ;({
-  '--tw-transform-scale-x': '1.1',
+  '--tw-scale-x': '1.1',
 })
 ;({
-  '--tw-transform-scale-x': '1.25',
+  '--tw-scale-x': '1.25',
 })
 ;({
-  '--tw-transform-scale-x': '1.5',
+  '--tw-scale-x': '1.5',
 })
 ;({
-  '--tw-transform-scale-y': '0',
+  '--tw-scale-y': '0',
 })
 ;({
-  '--tw-transform-scale-y': '.5',
+  '--tw-scale-y': '.5',
 })
 ;({
-  '--tw-transform-scale-y': '.75',
+  '--tw-scale-y': '.75',
 })
 ;({
-  '--tw-transform-scale-y': '.9',
+  '--tw-scale-y': '.9',
 })
 ;({
-  '--tw-transform-scale-y': '.95',
+  '--tw-scale-y': '.95',
 })
 ;({
-  '--tw-transform-scale-y': '1',
+  '--tw-scale-y': '1',
 })
 ;({
-  '--tw-transform-scale-y': '1.05',
+  '--tw-scale-y': '1.05',
 })
 ;({
-  '--tw-transform-scale-y': '1.1',
+  '--tw-scale-y': '1.1',
 })
 ;({
-  '--tw-transform-scale-y': '1.25',
+  '--tw-scale-y': '1.25',
 })
 ;({
-  '--tw-transform-scale-y': '1.5',
+  '--tw-scale-y': '1.5',
 }) // https://tailwindcss.com/docs/rotate
 
 ;({

src/config/dynamicStyles.js
@@ -345,10 +345,10 @@ export default {
    */
 
   // https://tailwindcss.com/docs/scale
-  'scale-x': { prop: '--tw-transform-scale-x', config: 'scale' },
-  'scale-y': { prop: '--tw-transform-scale-y', config: 'scale' },
+  'scale-x': { prop: '--tw-scale-x', config: 'scale' },
+  'scale-y': { prop: '--tw-scale-y', config: 'scale' },
   scale: {
-    prop: ['--tw-transform-scale-x', '--tw-transform-scale-y'],
+    prop: ['--tw-scale-x', '--tw-scale-y'],
     config: 'scale',
   },
 

src/config/staticStyles.js
@@ -703,15 +703,15 @@ export default {
 
   'transform-gpu': {
     output: {
-      '--transform-translate-x': '0',
-      '--transform-translate-y': '0',
-      '--transform-rotate': '0',
-      '--transform-skew-x': '0',
-      '--transform-skew-y': '0',
-      '--transform-scale-x': '1',
-      '--transform-scale-y': '1',
+      '--tw-translate-x': '0',
+      '--tw-translate-y': '0',
+      '--tw-rotate': '0',
+      '--tw-skew-x': '0',
+      '--tw-skew-y': '0',
+      '--tw-scale-x': '1',
+      '--tw-scale-y': '1',
       transform:
-        'translate3d(var(--transform-translate-x), var(--transform-translate-y), 0) rotate(var(--transform-rotate)) skewX(var(--transform-skew-x)) skewY(var(--transform-skew-y)) scaleX(var(--transform-scale-x)) scaleY(var(--transform-scale-y))',
+        'translate3d(var(--tw-translate-x), var(--tw-translate-y), 0) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))',
     },
   },
 