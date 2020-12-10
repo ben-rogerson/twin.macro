bd0c7e2dd18fb742c0493ef3c270ed28414976a5
Nate Radebaugh <nradebaugh@bdo.com>
2020-12-10 09:02:47 -0600

    Add types tests for linaria

---
+++ types/tests/linaria/index.tsx
@@ -0,0 +1,23 @@
+import { css } from '@linaria/core'
+import { styled } from '@linaria/react'
+import React from 'react'
+import tw from '../..'
+
+export const Container = styled.div`
+  ${tw`flex flex-col w-full max-w-sm`}
+  & > form {
+    ${tw`w-full`}
+  }
+`
+
+export const Link = tw.a``
+export const ComposedLink = tw(Link)``
+export const ComposedLink2 = styled(Link)``
+
+export const cssProperty = (
+  <div
+    className={css`
+      ${tw`bg-red-100`}
+    `}
+  />
+)
