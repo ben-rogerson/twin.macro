cf8c7956fb5c8376879e1f0ee03a22dfad1b541f
Ben Rogerson <info@benrogerson.com.au>
2021-01-20 17:19:15 +1030

    Add presets tests

---
+++ __fixtures__/presets/presets.js
@@ -0,0 +1,5 @@
+import tw from './macro'
+
+tw`text-badass`
+tw`text-banana`
+tw`text-hamburger`
--- plugin.test.js
+++ plugin.test.js
@@ -14,13 +14,15 @@ pluginTester({
     babelrc: true,
   },
   snapshot: true,
-  tests: glob.sync('__fixtures__/**/*.js').map(file => ({
-    title: path.basename(file),
-    code: fs.readFileSync(file, 'utf-8'),
-    ...(fs.existsSync(configFile(file)) && {
-      pluginOptions: {
-        twin: JSON.parse(fs.readFileSync(configFile(file), 'utf-8')),
-      },
-    }),
-  })),
+  tests: glob
+    .sync(['__fixtures__/**/*.js', '!__fixtures__/**/*.config.js'])
+    .map(file => ({
+      title: path.basename(file),
+      code: fs.readFileSync(file, 'utf-8'),
+      ...(fs.existsSync(configFile(file)) && {
+        pluginOptions: {
+          twin: JSON.parse(fs.readFileSync(configFile(file), 'utf-8')),
+        },
+      }),
+    })),
 })
