6f2b923738e3b6a0ef2cb3b56d0a305644f183bf
Ben Rogerson <info@benrogerson.com.au>
2022-04-24 21:44:34 +0930

    Update dev server info message

--- sandbox/in.js
+++ sandbox/in.js
@@ -4,9 +4,9 @@
  * Good for general testing or for development of new features.
  *
  * Getting started
- * 1. Run the script: "npm run dev:sandbox" (or "npm run dev:macro:sandbox" if deving)
- * 2. Make a change to this file
- * 3. Check 'out.js' for the macro output
+ * 1. Run the script: `npm run dev`
+ * 2. Make a change to this file or to a file in the src folder
+ * 3. Check `sandbox/out.js` for the macro output
  */
 
 // @ts-nocheck
