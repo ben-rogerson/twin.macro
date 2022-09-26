51384af14c32d9fefcbfdfe634a1954d4cf06df5
Ben Rogerson <info@benrogerson.com.au>
2022-09-27 06:28:31 +0930

    Fix shortCss check

--- src/core/lib/util/isShortCss.ts
+++ src/core/lib/util/isShortCss.ts
@@ -1,6 +1,6 @@
 import escapeRegex from './escapeRegex'
 
-const SHORT_CSS = /[^/-]\\\[(?!.+:)/
+const SHORT_CSS = /\w[^/-]\\\[(?!.+:)/
 
 export default function isShortCss(className: string): boolean {
   return SHORT_CSS.test(escapeRegex(className))
